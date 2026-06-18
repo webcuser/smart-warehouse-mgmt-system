42cac91234017c91ce6b8a13b06a659db61a6ee3
webcuser <97110714+webcuser@users.noreply.github.com>
2026-06-18 20:36:28 +0200

    feat: Setup Project Structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className="text-2xl font-bold">Smart Warehouse Management System</h1>
+    </div>
+  );
+};
+
+export default App;
